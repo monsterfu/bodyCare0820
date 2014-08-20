4a1d905386657f3a25f8834c9effda4ba0659411
monsterfu <seanh@seanHtekiMac-mini.local>
2014-08-20 10:33:46 +0800

    111
    11111

---
+++ bodyCareTests/bodyCareTests.m
@@ -0,0 +1,34 @@
+//
+//  bodyCareTests.m
+//  bodyCareTests
+//
+//  Created by Monster on 14-8-19.
+//  Copyright (c) 2014年 Monster. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface bodyCareTests : XCTestCase
+
+@end
+
+@implementation bodyCareTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
